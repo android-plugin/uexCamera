da28da4742b96d8a17798bf2277ee6b553b3c8ab
yajun.duan <yajun.duan@zymobi.com>
2017-09-18 16:23:03 +0800

    添加相机权限被拒回调,添加英文环境资源包

---
+++ src/org/zywx/wbpalmstar/plugin/uexcamera/utils/PermissionUtil.java
@@ -0,0 +1,27 @@
+package org.zywx.wbpalmstar.plugin.uexcamera.utils;
+
+import android.hardware.Camera;
+
+public class PermissionUtil {
+    public static boolean checkCameraPermission() {
+        boolean isCanUse = true;
+        Camera mCamera = null;
+        try {
+            mCamera = Camera.open();
+            Camera.Parameters mParameters = mCamera.getParameters(); //针对魅族手机
+            mCamera.setParameters(mParameters);
+        } catch (Exception e) {
+            isCanUse = false;
+        }
+
+        if (mCamera != null) {
+            try {
+                mCamera.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return isCanUse;
+            }
+        }
+        return isCanUse;
+    }
+}
