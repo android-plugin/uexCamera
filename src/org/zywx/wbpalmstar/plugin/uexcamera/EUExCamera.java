2e6e013d541062d7775083cb2d9299ea924561ff
张一鹏 <sandy1108@163.com>
2021-04-29 21:04:38 +0800

    处理了openInternal可能导致的闪退问题

--- src/org/zywx/wbpalmstar/plugin/uexcamera/EUExCamera.java
+++ src/org/zywx/wbpalmstar/plugin/uexcamera/EUExCamera.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.zywx.wbpalmstar.base.BDebug;
 import org.zywx.wbpalmstar.base.BUtility;
 import org.zywx.wbpalmstar.engine.DataHelper;
 import org.zywx.wbpalmstar.engine.EBrowserView;
@@ -833,26 +834,31 @@ public class EUExCamera extends EUExBase implements CallbackCameraViewClose {
                 if (finalPath != null) {
                     try {
                         exif = new ExifInterface(finalPath);
+                        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
+                        if (orientation != -1) {
+                            switch (orientation) {
+                                case ExifInterface.ORIENTATION_NORMAL:
+                                    degree = 0;
+                                    break;
+                                case ExifInterface.ORIENTATION_ROTATE_90:
+                                    degree = 90;
+                                    break;
+                                case ExifInterface.ORIENTATION_ROTATE_180:
+                                    degree = 180;
+                                    break;
+                                case ExifInterface.ORIENTATION_ROTATE_270:
+                                    degree = 270;
+                                    break;
+                            }
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
-                    if (orientation != -1) {
-                        switch (orientation) {
-                            case ExifInterface.ORIENTATION_NORMAL:
-                                degree = 0;
-                                break;
-                            case ExifInterface.ORIENTATION_ROTATE_90:
-                                degree = 90;
-                                break;
-                            case ExifInterface.ORIENTATION_ROTATE_180:
-                                degree = 180;
-                                break;
-                            case ExifInterface.ORIENTATION_ROTATE_270:
-                                degree = 270;
-                                break;
-                        }
+
+                    if (mTempPath != null && !mTempPath.exists()){
+                        BDebug.e(TAG, "openInternal Camera mTempPath is not exist: " + mTempPath.getAbsolutePath());
                     }
+
                     if (!mIsCompress && 0 == degree) {
                         if (TextUtils.isEmpty(openInternalFunc)) {
                             jsCallback(FUNC_OPEN_INTERNAL_CALLBACK, 0, EUExCallback.F_C_TEXT, finalPath);
